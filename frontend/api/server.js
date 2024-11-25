96ce407fd533eb08fc32377ef43fd9402c879950
lybugproducer <1743055094@qq.com>
2024-11-25 22:27:43 +0800

    241125

--- frontend/api/server.js
+++ frontend/api/server.js
@@ -1,5 +1,5 @@
-const host = "http://127.0.0.1:8080"
+// const host = "http://127.0.0.1:8080"
 // const host = 'http://127.0.0.1:4523/m1/4980076-4638643-default';
-// const host = 'https://demo.grtsinry43.com/xiaokang/api';
+const host = 'https://demo.grtsinry43.com/xiaokang';
 
 export default host;
